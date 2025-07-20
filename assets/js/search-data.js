d6f56d2ba54a1045a756c0e4adfad2f8dab2bc49
azaksm <azaksm@users.noreply.github.com>
2025-07-20 22:32:50 +0000

    deploy: 0fd59896440af0a6aae8dc4653868a0701d3ff35

---
+++ assets/js/search-data.js
@@ -0,0 +1,35 @@
+// get the ninja-keys element
+const ninja = document.querySelector('ninja-keys');
+
+// add the home and posts menu items
+ninja.data = [{
+    id: "nav-home",
+    title: "home",
+    section: "Navigation",
+    handler: () => {
+      window.location.href = "/";
+    },
+  },{id: "nav-blog",
+          title: "blog",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/blog/";
+          },
+        },{id: "post-introduction",
+        
+          title: "Introduction",
+        
+        description: "Вводный пост",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2025/introduction/";
+          
+        },
+      },{id: "books-the-godfather",
+          title: 'The Godfather',
+          description: "",
+          section: "Books",handler: () => {
+              window.location.href = "/books/the_godfather/";
+            },},];
