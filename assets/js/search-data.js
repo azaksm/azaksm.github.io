6a002b2d17da785b1d3e68a4139a5e95dcf7b80d
azaksm <azaksm@users.noreply.github.com>
2025-07-21 00:35:12 +0000

    deploy: b8532f7cf148db694ec1407596720c5948fa7519

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -16,7 +16,18 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/blog/";
           },
-        },{id: "post-предисловие",
+        },{id: "post-конференция-в-лиссабоне",
+        
+          title: "Конференция в Лиссабоне",
+        
+        description: "Ноябрь",
+        section: "Posts",
+        handler: () => {
+          
+            window.location.href = "/blog/2025/conference/";
+          
+        },
+      },{id: "post-предисловие",
         
           title: "Предисловие",
         
@@ -32,4 +43,7 @@ ninja.data = [{
           description: "",
           section: "Books",handler: () => {
               window.location.href = "/books/the_godfather/";
-            },},];
+            },},{id: "news-день-рождения-сайта",
+          title: 'День рождения сайта!',
+          description: "",
+          section: "News",},];
