624cecf5e7ac53f7b00b2abfd62a4fbb7fdfb485
azaksm <azaksm@users.noreply.github.com>
2026-03-19 13:55:33 +0000

    deploy: a6ff269e6f4182d6f735ffe3c71856130bda9f75

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,14 +9,7 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-blog",
-          title: "blog",
-          description: "",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/blog/";
-          },
-        },{id: "post-физика",
+  },{id: "post-физика",
         
           title: "Физика",
         
